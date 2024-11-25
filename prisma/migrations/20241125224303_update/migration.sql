4e2222b6eca52d0c8ec6aabbf976b80699c48ef4
Barum Niazi <96373839+Barum-Niazi@users.noreply.github.com>
2024-11-26 03:54:18 +0500

    More schema updates

---
+++ prisma/migrations/20241125224303_update/migration.sql
@@ -0,0 +1,43 @@
+-- CreateEnum
+CREATE TYPE "NotificationType" AS ENUM ('SYSTEM', 'FEEDBACK', 'MEETING', 'OKR', 'GENERAL');
+
+-- CreateEnum
+CREATE TYPE "NotificationStatus" AS ENUM ('UNREAD', 'READ');
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "designationId" INTEGER,
+ADD COLUMN     "isAdmin" BOOLEAN NOT NULL DEFAULT false;
+
+-- CreateTable
+CREATE TABLE "Notification" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "type" "NotificationType" NOT NULL,
+    "message" TEXT NOT NULL,
+    "status" "NotificationStatus" NOT NULL DEFAULT 'UNREAD',
+    "link" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Notification_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Designation" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Designation_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Designation_title_key" ON "Designation"("title");
+
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_designationId_fkey" FOREIGN KEY ("designationId") REFERENCES "Designation"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Notification" ADD CONSTRAINT "Notification_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
