3213d9ffc7fa417500eb35b68d54226a3e9fe31b
Barum Niazi <96373839+Barum-Niazi@users.noreply.github.com>
2025-04-22 04:16:46 +0500

    Update auth.service.ts

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -69,7 +69,7 @@ export class AuthService {
     const payload = {
       sub: user.id,
       email: user.email,
-      roles: user.roles, // ✅ This is now an array
+      roles: user.roles,
       companyId: user.companyId,
     };
 
